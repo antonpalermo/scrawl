94d710e6869e5f2e9b34e984f56e4786e9f1d411
antonpalermo <ANPBusinessmail@gmail.com>
2023-04-15 22:38:33 +0800

    added env validations

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,15 +2,19 @@ import type { NextAuthOptions } from "next-auth"
 
 import GoogleProvider from "next-auth/providers/google"
 
+import { env } from "@scrawl/env.mjs"
 import { prisma } from "@scrawl/server/prisma"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
+/**
+ * next-auth options configuration.
+ */
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
     })
   ],
   session: {
