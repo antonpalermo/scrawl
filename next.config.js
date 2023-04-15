f712d7b3508209585d5a70ff11efdc62138b85b4
antonpalermo <ANPBusinessmail@gmail.com>
2023-04-15 22:16:02 +0800

    added linter

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type { import('next').NextConfig }
+ */
+const config = {
+  reactStrictMode: true,
+  devIndicators: {
+    buildActivity: false
+  }
+}
+
+module.exports = config
